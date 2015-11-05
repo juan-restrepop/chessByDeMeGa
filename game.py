7cc124f33a77e198b3b4f60508b599be0cb0dfc0
FranciscoRG <rodriguezgomez.fj@gmail.com>
2015-11-05 17:49:07 +0100

    added black pawns

--- game.py
+++ game.py
@@ -11,7 +11,8 @@ class Pawn(object):
 
 class Game(object):
     grid = list()
-    pawns = list()
+    pawns_w = list()
+    pawns_b = list()
 
     def __init__(self):
         self.initialize_board()
@@ -19,14 +20,15 @@ class Game(object):
         self.initialize_board_with_pieces()
 
     def initialize_board_with_pieces(self):
-        for p in self.pawns:
+        for p in (self.pawns_w + self.pawns_b):
             rowCoord = p.coordinates[0]
             colCoord = p.coordinates[1]
             self.grid[rowCoord][colCoord] = p.kind
 
     def initialize_pieces(self):
         for k in range(8):
-            self.pawns.append(Pawn('w', [1, k]))
+            self.pawns_w.append(Pawn('w', [1, k]))
+            self.pawns_b.append(Pawn('b', [6, k]))
 
 
     def initialize_board(self):
