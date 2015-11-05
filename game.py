eca712ed2b1d3481f1d747d2e93a003f77c8ba87
Juan Restrepo <juan.restrepop@gmail.com>
2015-11-05 16:42:53 +0100

    Make Game class

--- game.py
+++ game.py
@@ -8,3 +8,26 @@ def print_board(board_size):
 
     print board_string
 
+
+class Game(object):
+    grid = list()
+
+    def __init__(self):
+        for i in range(0,8):
+            self.grid.append(list())
+
+            for j in range(0, 8):
+                self.grid[i].append(str((j + i) % 2))
+
+    def print_board(self):
+
+        board_string = ''
+        for i in range(0, 8):
+            for j in range(0, 8):
+                board_string = board_string + ' ' + self.grid[i][j]
+
+            board_string = board_string + '\n'
+
+        print board_string
+
+
