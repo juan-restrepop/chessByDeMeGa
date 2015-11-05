4f510c1e53912ff71e0da43d02dd1452f1728009
Juan Restrepo <juan.restrepop@gmail.com>
2015-11-05 16:44:38 +0100

    erase unnecessary function

--- game.py
+++ game.py
@@ -1,14 +1,3 @@
-def print_board(board_size):
-    board_string = ''
-    for i in range(0, board_size):
-        for j in range(0, board_size):
-            board_string = board_string + str( (j + i) % 2)
-
-        board_string = board_string + '\n'
-
-    print board_string
-
-
 class Game(object):
     grid = list()
 
