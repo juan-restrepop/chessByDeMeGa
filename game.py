aa06a1d7c3f4bbe0638d7c6413f71b4d4dc8885f
FranciscoRG <rodriguezgomez.fj@gmail.com>
2015-11-05 18:23:38 +0100

    add knights,bishops,rooks

--- game.py
+++ game.py
@@ -4,10 +4,29 @@ class Pawn(pieces.Piece):
     def __init__(self, color, coordinates):
         pieces.Piece.__init__(self, 'p', color, coordinates)
 
+class Rook(pieces.Piece):
+    def __init__(self, color, coordinates):
+        pieces.Piece.__init__(self, 'r', color, coordinates)
+
+class Bishop(pieces.Piece):
+    def __init__(self, color, coordinates):
+        pieces.Piece.__init__(self, 'b', color, coordinates)
+
+class Knight(pieces.Piece):
+    def __init__(self, color, coordinates):
+        pieces.Piece.__init__(self, 'n', color, coordinates)
+
+
 class Game(object):
     grid = list()
     pawns_w = list()
     pawns_b = list()
+    rooks_w = list()
+    rooks_b = list()
+    bishops_w = list()
+    bishops_b = list()
+    knights_w = list()
+    knights_b = list()
 
     def __init__(self):
         self.initialize_board()
@@ -15,7 +34,10 @@ class Game(object):
         self.initialize_board_with_pieces()
 
     def initialize_board_with_pieces(self):
-        for p in (self.pawns_w + self.pawns_b):
+        for p in (self.pawns_w + self.pawns_b
+                + self.rooks_w + self.rooks_b
+                + self.bishops_w + self.bishops_b
+                + self.knights_w + self.knights_b):
             rowCoord = p.coordinates[0]
             colCoord = p.coordinates[1]
             self.grid[rowCoord][colCoord] = p.kind
@@ -24,6 +46,25 @@ class Game(object):
         for k in range(8):
             self.pawns_w.append(Pawn('w', [1, k]))
             self.pawns_b.append(Pawn('b', [6, k]))
+           
+        self.rooks_w.append(Rook('w',[0,0]))
+        self.rooks_w.append(Rook('w',[0,7]))
+        
+        self.bishops_w.append(Bishop('w',[0,2]))
+        self.bishops_w.append(Bishop('w',[0,5]))
+
+        self.knights_w.append(Knight('w',[0,1]))
+        self.knights_w.append(Knight('w',[0,6]))
+        
+        self.rooks_b.append(Rook('b',[7,0]))
+        self.rooks_b.append(Rook('b',[7,7]))
+        
+        self.bishops_b.append(Bishop('b',[7,2]))
+        self.bishops_b.append(Bishop('b',[7,5]))
+
+        self.knights_b.append(Knight('b',[7,1]))
+        self.knights_b.append(Knight('b',[7,6]))
+
 
 
     def initialize_board(self):
