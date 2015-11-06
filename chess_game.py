db535fd1a147f86f4dff9693cf87d3865505d792
FranciscoRG <rodriguezgomez.fj@gmail.com>
2015-11-06 13:23:57 +0100

    add quit to chess game

--- chess_game.py
+++ chess_game.py
@@ -6,21 +6,18 @@ class ChessGame(object):
         self.board = chess_board.ChessBoard()
 
     def run(self):
-        while(True):
+        stay_in_game = True
+        while(stay_in_game):
             self.board.print_board()
-            self.read_user_move()        
+            stay_in_game = self.read_user_move()        
 
     def read_user_move(self):
-        new_move_str = raw_input("Please enter a new move: \n")
-        self.parse_user_move(new_move_str)
+        new_move_str = raw_input("Please enter a new move: \ntype 'q' to quit the game\n")
+        
+        return(self.parse_user_move(new_move_str))
 
     def parse_user_move(self, input_move):
+        if input_move == "q":
+            return False
         print("Your move is : "+input_move)
-        
-        
-
-
-
-
-
-
+        return True
