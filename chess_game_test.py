7b88937ea68e5ea64b6c5e6b5c0e4ecf71dd7b10
FranciscoRG <rodriguezgomez.fj@gmail.com>
2015-11-07 14:57:53 +0100

    test is pawn

--- chess_game_test.py
+++ chess_game_test.py
@@ -5,13 +5,33 @@ class TestChessGame(unittest.TestCase):
 
     def test_has_quit(self):
         c  = cg.ChessGame()
-        
+
         expected = True
         actual = c.has_quit('q')
         self.assertEqual(expected, actual)
         
         expected = False
         actual = c.has_quit('bla')
+        self.assertEqual(expected, actual)
+
+    def test_is_pawn(self):
+        c  = cg.ChessGame()
+        
+        expected = True
+        for col in c.column_names:
+            
+            actual = c.is_pawn(col +'1')
+            self.assertEqual(expected, actual)
+
+            actual = c.is_pawn(col + 'x1d')
+            self.assertEqual(expected, actual)
+
+        expected = False
+        actual = c.is_pawn('B1')
+        self.assertEqual(expected, actual)
+        
+        expected = False
+        actual = c.is_pawn('Kx1')
         self.assertEqual(expected, actual)
 
 if __name__ == '__main__':
