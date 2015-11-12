83d72bdd005d03b805e899ea58301dae0a62e0ac
Juan Restrepo <juan.restrepop@gmail.com>
2015-11-12 14:21:39 +0100

    Fix pieces initialization to match standard chess notations

--- chess_board.py
+++ chess_board.py
@@ -95,32 +95,32 @@ class ChessBoard(object):
         self.clean_pieces()
 
         for k in range(8):
-            self.pawns_w.append(pieces.Pawn('w', [1, k]))
-            self.pawns_b.append(pieces.Pawn('b', [6, k]))
+            self.pawns_w.append(pieces.Pawn('w', [6, k]))
+            self.pawns_b.append(pieces.Pawn('b', [1, k]))
            
-        self.rooks_w.append(pieces.Rook('w',[0,0]))
-        self.rooks_w.append(pieces.Rook('w',[0,7]))
+        self.rooks_w.append(pieces.Rook('w',[7,0]))
+        self.rooks_w.append(pieces.Rook('w',[7,7]))
         
-        self.bishops_w.append(pieces.Bishop('w',[0,2]))
-        self.bishops_w.append(pieces.Bishop('w',[0,5]))
+        self.bishops_w.append(pieces.Bishop('w',[7,2]))
+        self.bishops_w.append(pieces.Bishop('w',[7,5]))
 
-        self.knights_w.append(pieces.Knight('w',[0,1]))
-        self.knights_w.append(pieces.Knight('w',[0,6]))
+        self.knights_w.append(pieces.Knight('w',[7,1]))
+        self.knights_w.append(pieces.Knight('w',[7,6]))
         
-        self.rooks_b.append(pieces.Rook('b',[7,0]))
-        self.rooks_b.append(pieces.Rook('b',[7,7]))
+        self.rooks_b.append(pieces.Rook('b',[0,0]))
+        self.rooks_b.append(pieces.Rook('b',[0,7]))
         
-        self.bishops_b.append(pieces.Bishop('b',[7,2]))
-        self.bishops_b.append(pieces.Bishop('b',[7,5]))
+        self.bishops_b.append(pieces.Bishop('b',[0,2]))
+        self.bishops_b.append(pieces.Bishop('b',[0,5]))
 
-        self.knights_b.append(pieces.Knight('b',[7,1]))
-        self.knights_b.append(pieces.Knight('b',[7,6]))
+        self.knights_b.append(pieces.Knight('b',[0,1]))
+        self.knights_b.append(pieces.Knight('b',[0,6]))
 
-        self.king_b.append(pieces.King('b', [7, 4]))
-        self.king_w.append(pieces.King('w', [0, 4]))
+        self.king_b.append(pieces.King('b', [0, 4]))
+        self.king_w.append(pieces.King('w', [7, 4]))
 
-        self.queen_b.append(pieces.Queen('b', [7, 3]))
-        self.queen_w.append(pieces.Queen('w', [0, 3]))
+        self.queen_b.append(pieces.Queen('b', [0, 3]))
+        self.queen_w.append(pieces.Queen('w', [7, 3]))
 
     def initialize_board_with_pieces(self):
         for p in self.get_all_pieces():
