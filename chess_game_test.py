22068aa64766a6858690530140a2fda933fcfde2
Juan Restrepo <juan.restrepop@gmail.com>
2015-11-23 22:02:27 +0100

    Fixes names in `chess_game_test.py`

--- chess_game_test.py
+++ chess_game_test.py
@@ -71,46 +71,48 @@ class TestChessGame(unittest.TestCase):
     def test_validate_eat_case(self):
         c  = cg.ChessGame()
         expected = True
-        for input_move in ['Kxf3','axb2','Rxd2','Rxd2']:
+        for input_move in ['Kxf3', 'axb2', 'Rxd2']:
 
             actual = c.validate_eat_case(input_move)
             self.assertEqual(expected, actual)
 
         expected = False
-        for input_move in ['Kf3','a2','Rxd']:
+        for input_move in ['Kf3', 'a2', 'Rxd']:
 
             actual = c.validate_eat_case(input_move)
             self.assertEqual(expected, actual)
 
-    def test_validate_normal_case(self):
+    def test_validate_move_case(self):
         c  = cg.ChessGame()
 
         expected = True
         for piece in ['K','Q','N','B','R']:
-            actual = c.validate_normal_case(piece +'f3')
+            actual = c.validate_move_case(piece +'f3')
             self.assertEqual(expected, actual)
 
         expected = True
         for col in c.column_names:
-            actual = c.validate_normal_case(col +'3')
+            actual = c.validate_move_case(col +'3')
             self.assertEqual(expected, actual)
 
         expected = False
         for piece in ['K','Q','N','B','R']:
-            actual = c.validate_normal_case(piece +'3')
+            actual = c.validate_move_case(piece +'3')
             self.assertEqual(expected, actual)
 
         for col in c.column_names:
-            actual = c.validate_normal_case(col)
+            actual = c.validate_move_case(col)
             self.assertEqual(expected, actual)
 
     def test_is_user_move_valid(self):
         c  = cg.ChessGame()
 
         expected = True
+        k = 1
         for input_move in ['a2','axb2','Bf3','Bxa2']:
             actual = c.is_user_move_valid(input_move)
-            self.assertEqual(expected, actual)
+            self.assertEqual(expected, actual, msg='Error on test # %i: %s' % (k, input_move))
+            k += 1
 
         expected = False
         k = 1
