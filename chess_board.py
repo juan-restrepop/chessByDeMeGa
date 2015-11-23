e9278ff57807da4785b9dfacdf1d5067c3a695c0
Juan Restrepo <juan.restrepop@gmail.com>
2015-11-23 16:39:07 +0100

    Refactor queen movement rules

--- chess_board.py
+++ chess_board.py
@@ -174,9 +174,6 @@ class ChessBoard(object):
 
 
 
-    def can_queen_reach(self, i, j, queen):
-        return self.can_bishop_reach(i, j, queen) or self.can_rook_reach(i, j, queen)
-
     def can_knight_reach(self, i, j, knight):
         # TODO: The knight should eat if final square is occupied
         i_origin, j_origin = knight.coordinates
@@ -281,7 +278,7 @@ class ChessBoard(object):
         queen_list = self.list_to_update(player,self.queen_w,self.queen_b)
 
         for k in range(len(queen_list)):
-            if self.can_queen_reach(i, j, queen_list[k]):
+            if self.Rules.is_queen_movement_valid(self, i, j, queen_list[k]):
                 self.piece_mover(queen_list[k], i, j)
                 accepted_move = True
                 break
@@ -454,5 +451,8 @@ class MovementRules(object):
             return False
 
         return free_path # to liberty
+
+    def is_queen_movement_valid(self, board, i, j, queen):
+        return self.is_rook_movement_valid(board, i, j, queen) or self.is_bishop_movement_valid(board, i, j, queen)
 
 
