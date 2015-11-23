9879c30713cecf74202406baace64fe9b0cb9509
Juan Restrepo <juan.restrepop@gmail.com>
2015-11-23 18:10:26 +0100

    Adds `print_big_cursed_board()` which allozs for a nicer visualization.

--- chess_cursed_visualizer.py
+++ chess_cursed_visualizer.py
@@ -5,8 +5,8 @@ def rgb_to_curses_color(color):
      return color[0]*1000/255, color[1]*1000/255, color[2]*1000/255
 
 ## Colors for display
-white_square_rgb = (255, 0, 0)
-black_square_rgb = (0, 0, 255)
+white_square_rgb = (145, 120, 0)
+black_square_rgb = (78, 60, 18)
 white_piece_rgb = (0, 0, 0)
 black_piece_rgb = (255, 255, 255)
 
@@ -16,23 +16,23 @@ curses.start_color()
 height, width = screen.getmaxyx()
 
 ## Initialize colors
-# Board  white squares
+## Board  white squares
 curses.init_color(1, rgb_to_curses_color(white_square_rgb)[0], rgb_to_curses_color(white_square_rgb)[1], rgb_to_curses_color(white_square_rgb)[2])
-# Board black squares
+## Board black squares
 curses.init_color(2, rgb_to_curses_color(black_square_rgb)[0], rgb_to_curses_color(black_square_rgb)[1], rgb_to_curses_color(black_square_rgb)[2])
-# White piece
+## White piece
 curses.init_color(3, rgb_to_curses_color(white_piece_rgb)[0], rgb_to_curses_color(white_piece_rgb)[1], rgb_to_curses_color(white_piece_rgb)[2])
-# Black piece
+## Black piece
 curses.init_color(4, rgb_to_curses_color(black_piece_rgb)[0], rgb_to_curses_color(black_piece_rgb)[1], rgb_to_curses_color(black_piece_rgb)[2])
 
 ## Initialize necessary color pairs
-# Black piece on white square
+## Black piece on white square
 curses.init_pair(1, 4, 1)
-# Black piece on black square
+## Black piece on black square
 curses.init_pair(2, 4, 2)
-# White piece on white square
+## White piece on white square
 curses.init_pair(3, 3, 1)
-# White piece on black square
+## White piece on black square
 curses.init_pair(4, 3, 2)
 
 def get_piece_color(piece):
@@ -67,6 +67,33 @@ def screen_small_empty_board(Board):
 
     screen.move(height - 1, width - 1)
 
+def screen_big_empty_board(Board, square_width=5, square_height=2):
+    screen.clear()
+
+    board_columns = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']
+    board_lines = ['8', '7', '6', '5', '4', '3', '2', '1']
+
+    i_0, j_0 = 1, 2
+
+    for i in range(8):
+        for j in range(8):
+            screen.addstr(0, 1 + j_0 + j*square_width, board_columns[j])
+            screen.addstr(i_0 + i * square_height, 0, board_lines[i])
+            color = Board.get_square_color(i, j) + 1
+            for k in range(square_height):
+                screen.addstr(i_0 + i*square_height + k, j_0 + j * square_width, ' '*square_width, curses.color_pair(color))
+
+    screen.move(height - 1, width - 1)
+
+
+def board_coord_to_big_screen_coords(i, j, square_width=5, square_height=2):
+    I = i * square_height
+    J = j * square_width + 2
+
+    return I, J
+
+
+
 def print_small_cursed_board(Board):
 
     screen_small_empty_board(Board)
@@ -77,6 +104,27 @@ def print_small_cursed_board(Board):
         color = get_piece_color(piece)
 
         screen.addstr(i_0 + i, j_0 + j, piece.kind, curses.color_pair(color))
+
+    screen.move(height - 1, width - 1)
+    screen.refresh()
+    screen.getch()
+    close_cursed_screen()
+
+def print_big_cursed_board(Board):
+
+    screen_big_empty_board(Board)
+
+    i_0, j_0 = 1, 2
+    for piece in Board.get_all_pieces():
+        i, j = piece.coordinates
+        if piece.color == 'w':
+            I, J = board_coord_to_big_screen_coords(i, j)
+            I = I + 1
+        else:
+            I, J = board_coord_to_big_screen_coords(i, j)
+        color = get_piece_color(piece)
+
+        screen.addstr(i_0 + I, j_0 + J, piece.kind, curses.color_pair(color))
 
     screen.move(height - 1, width - 1)
     screen.refresh()
