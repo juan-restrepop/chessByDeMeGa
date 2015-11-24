eb3b194574141d46e4b676aa515bb6ee86d67e7c
Juan Restrepo <juan.restrepop@gmail.com>
2015-11-24 18:36:29 +0100

    Adds `is_diagonal_move_valid()`

--- chess_board.py
+++ chess_board.py
@@ -355,6 +355,50 @@ class MovementRules(object):
             return False
         return free_path# to liberty
 
+    def is_diagonal_move_valid(self, board, i_origin, j_origin, i_end, j_end):
+        if abs(i_end - i_origin) and (j_end - j_origin):
+            free_path = True
+
+            if (i_end > i_origin) and (j_end > j_origin):# move down and to the right
+                temp_i = i_origin + 1
+                temp_j = j_origin + 1
+                while free_path and (temp_i <= i_end):
+                    free_path = board.is_square_free(temp_i, temp_j)
+                    temp_i += 1
+                    temp_j += 1
+
+            elif (i_end > i_origin) and (j_end < j_origin):# move down and to the left
+                temp_i = i_origin + 1
+                temp_j = j_origin - 1
+                while free_path and (temp_i <= i_end):
+                    free_path = board.is_square_free(temp_i, temp_j)
+                    temp_i += 1
+                    temp_j -= 1
+
+            elif (i_end < i_origin) and (j_end < j_origin):# move up and to the left
+                temp_i = i_origin - 1
+                temp_j = j_origin - 1
+                while free_path and (temp_i >= i_end):
+                    free_path = board.is_square_free(temp_i, temp_j)
+                    temp_i -= 1
+                    temp_j -= 1
+
+            elif (i_end < i_origin) and (j_end > j_origin):# move up and to the right
+                temp_i = i_origin - 1
+                temp_j = j_origin + 1
+                while free_path and (temp_i >= i_end):
+                    free_path = board.is_square_free(temp_i, temp_j)
+                    temp_i -= 1
+                    temp_j += 1
+
+            return free_path
+        else:
+            return False
+
+
+
+        return free_path
+
     ## Simple movement rules
     def is_pawn_movement_valid(self, board, i, j, pawn, player = 'white'):
         i_origin, j_origin = pawn.coordinates
