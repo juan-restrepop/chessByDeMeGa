df79df981e7ea1b607e78800a6777ae3bfc94e15
FranciscoRG <rodriguezgomez.fj@gmail.com>
2015-12-07 08:58:52 +0100

    look for black king checked in game also

--- chess_game.py
+++ chess_game.py
@@ -157,7 +157,8 @@ class ChessGame(object):
 
         accepted_move = self.move_piece_to(input_move, move_to_col, move_to_line)
 
-        self.board.Rules.is_king_under_attack(self.board) #prints when any king is checked
+        self.board.Rules.is_king_under_attack(self.board) #prints when white king is checked
+        self.board.Rules.is_king_under_attack(self.board,'black') #when black king is checked
         if accepted_move:
             out_str = self.print_move(input_move, move_to_col, move_to_line)
             print("Your move is : " + input_move + '. ' + out_str)
