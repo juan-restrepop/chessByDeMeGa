63a23ea90f8a02114d6cf78120ef376d31375c38
Juan Restrepo <juan.restrepop@gmail.com>
2016-01-27 17:55:04 +0100

    Adds

--- chess_board_test.py
+++ chess_board_test.py
@@ -2131,6 +2131,99 @@ class TestChessBoard(unittest.TestCase):
         actual = b.color_augmented_grid()
         self.assertEqual(expected, actual, msg="The sacrifice of the white bishop to 'f5' is not enough")
 
+    def test_black_king_out_of_check(self):
+        b = cb.ChessBoard()
+        b_ref = cb.ChessBoard()
+        b.clean_pieces()
+        b_ref.clean_pieces()
+
+        # Test valid escape from single check
+        b.initialize_single_piece('k', 'b', b.transform_board_to_grid('d', '3'))
+        b.initialize_single_piece('q', 'w', b.transform_board_to_grid('g', '6'))
+
+        b.piece_mover('k', 'd', '4', 'black')
+
+        b_ref.initialize_single_piece('k', 'b', b_ref.transform_board_to_grid('d', '4'))
+        b_ref.initialize_single_piece('q', 'w', b_ref.transform_board_to_grid('g', '6'))
+
+        expected = b_ref.color_augmented_grid()
+        actual = b.color_augmented_grid()
+
+        self.assertEqual(expected, actual, msg="Black king should be allowed to escape to 'd4'")
+
+        # Test valid escape plan by protecting piece
+        b.clean_pieces()
+        b_ref.clean_pieces()
+
+        b.initialize_single_piece('k', 'b', b.transform_board_to_grid('d', '3'))
+        b.initialize_single_piece('b', 'b', b.transform_board_to_grid('d', '7'))
+        b.initialize_single_piece('q', 'w', b.transform_board_to_grid('g', '6'))
+
+        b.piece_mover('b', 'f', '5', 'black')
+
+        b_ref.initialize_single_piece('k', 'b', b_ref.transform_board_to_grid('d', '3'))
+        b_ref.initialize_single_piece('b', 'b', b_ref.transform_board_to_grid('f', '5'))
+        b_ref.initialize_single_piece('q', 'w', b_ref.transform_board_to_grid('g', '6'))
+
+        expected = b_ref.color_augmented_grid()
+        actual = b.color_augmented_grid()
+        self.assertEqual(expected, actual, msg="The sacrifice of the black bishop to 'f5' saves the king")
+
+        # Test invalid escape from single check
+        b.clean_pieces()
+        b_ref.clean_pieces()
+        b.initialize_single_piece('k', 'b', b.transform_board_to_grid('d', '3'))
+        b.initialize_single_piece('q', 'w', b.transform_board_to_grid('g', '6'))
+
+        b.piece_mover('k', 'c', '2', 'black')
+
+        b_ref.initialize_single_piece('k', 'b', b_ref.transform_board_to_grid('d', '3'))
+        b_ref.initialize_single_piece('q', 'w', b_ref.transform_board_to_grid('g', '6'))
+
+        expected = b_ref.color_augmented_grid()
+        actual = b.color_augmented_grid()
+
+        self.assertEqual(expected, actual, msg="Black king should not be allowed to escape to 'c2'")
+
+        # Test invalid from one check to another
+        b.clean_pieces()
+        b_ref.clean_pieces()
+
+        b.initialize_single_piece('k', 'b', b.transform_board_to_grid('d', '3'))
+        b.initialize_single_piece('q', 'w', b.transform_board_to_grid('g', '6'))
+        b.initialize_single_piece('r', 'w', b.transform_board_to_grid('a', '4'))
+
+        b.piece_mover('k', 'd', '4', 'black')
+
+        b_ref.initialize_single_piece('k', 'b', b_ref.transform_board_to_grid('d', '3'))
+        b_ref.initialize_single_piece('q', 'w', b_ref.transform_board_to_grid('g', '6'))
+        b_ref.initialize_single_piece('r', 'w', b_ref.transform_board_to_grid('a', '4'))
+
+        expected = b_ref.color_augmented_grid()
+        actual = b.color_augmented_grid()
+
+        self.assertEqual(expected, actual, msg="Black king should not be allowed to escape to 'd4', he is under attack by a rook")
+
+        # Test invalid escape plan by protecting piece
+        b.clean_pieces()
+        b_ref.clean_pieces()
+
+        b.initialize_single_piece('k', 'b', b.transform_board_to_grid('d', '3'))
+        b.initialize_single_piece('b', 'b', b.transform_board_to_grid('d', '7'))
+        b.initialize_single_piece('q', 'w', b.transform_board_to_grid('g', '6'))
+        b.initialize_single_piece('r', 'w', b.transform_board_to_grid('d', '8'))
+
+        b.piece_mover('b', 'f', '5', 'black')
+
+        b_ref.initialize_single_piece('k', 'b', b_ref.transform_board_to_grid('d', '3'))
+        b_ref.initialize_single_piece('b', 'b', b_ref.transform_board_to_grid('d', '7'))
+        b_ref.initialize_single_piece('q', 'w', b_ref.transform_board_to_grid('g', '6'))
+        b_ref.initialize_single_piece('r', 'w', b_ref.transform_board_to_grid('d', '8'))
+
+        expected = b_ref.color_augmented_grid()
+        actual = b.color_augmented_grid()
+        self.assertEqual(expected, actual, msg="The sacrifice of the black bishop to 'f5' is not enough")
+
 # Test castling
 
     def test_white_king_short_castling_rules(self):
