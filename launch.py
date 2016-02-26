60932b26f77cf8f224537a67a50cff82d8c56bd3
FranciscoRG <rodriguezgomez.fj@gmail.com>
2016-02-26 03:36:56 +0100

    handle optional filename argument in launch.py

--- launch.py
+++ launch.py
@@ -1,5 +1,15 @@
+import sys
+import pgn_reader
 import chess_game
 
 if __name__ == '__main__':
-    c = chess_game.ChessGame()
-    c.run()
+	
+	c = chess_game.ChessGame()
+	
+	if len (sys.argv[0])>1:
+
+		reader = pgn_reader.pgnBasicReader()
+		c.run( reader.play_generator(sys.argv[1]) )
+	
+	else:
+		c.run()
