346119d28839bfcb388101b51cf3f8613c278acb
FranciscoRG <rodriguezgomez.fj@gmail.com>
2016-03-01 22:42:00 +0100

    refactor launch

--- launch.py
+++ launch.py
@@ -2,24 +2,28 @@ import sys
 import pgn_reader
 import chess_game
 
-if __name__ == '__main__':
-    
+def run_chess():
     c = chess_game.ChessGame()
     
-    if len (sys.argv)>1:
-        try:
-            reader = pgn_reader.pgnBasicReader()
-            c.run( reader.play_generator(sys.argv[1]) )
-
-        except IOError as e:
-            print "Error #%s, "%e.errno,
-            print e.strerror
-
-        except EOFError as e:
-            print "Ctrl-d: User quit"
-            
-        except Exception:
-            raise
-
-    else:
+    if len (sys.argv)<=1:
         c.run()
+        return
+
+    try:
+        reader = pgn_reader.pgnBasicReader()
+        c.run( reader.play_generator(sys.argv[1]) )
+
+    except IOError as e:
+        print "Error #%s, "%e.errno,
+        print e.strerror
+
+    except EOFError as e:
+        print "Ctrl-d: User quit"
+        
+    except Exception:
+        raise
+
+if __name__ == '__main__':
+    run_chess()
+    
+
