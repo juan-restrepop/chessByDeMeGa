e8f0330cd259d14f8890ab91e8de2e6d580cec55
FranciscoRG <rodriguezgomez.fj@gmail.com>
2016-03-08 18:19:33 +0100

    fix test module

--- chess_validator_test.py
+++ chess_validator_test.py
@@ -4,13 +4,18 @@ import unittest
 import utils
 import chess_validator as validator
 
-def test_are_coordinates_valid(self):
-    expected = True
-    for coords in [('a', '1'), ('d', '5'), ('h', '8')]:
-        actual = validator.are_coordinates_valid(coords[0], coords[1])
-        self.assertEqual(expected, actual)
+class TestValidator(unittest.TestCase):
 
-    expected = False
-    for coords in [('a', '-1'), ('z', '5'), ('z', '17'), ('A', '5')]:
-        actual = validator.are_coordinates_valid(coords[0], coords[1])
-        self.assertEqual(expected, actual)
+    def test_are_coordinates_valid(self):
+        expected = True
+        for coords in [('a', '1'), ('d', '5'), ('h', '8')]:
+            actual = validator.are_coordinates_valid(coords[0], coords[1])
+            self.assertEqual(expected, actual)
+
+        expected = False
+        for coords in [('a', '-1'), ('z', '5'), ('z', '17'), ('A', '5')]:
+            actual = validator.are_coordinates_valid(coords[0], coords[1])
+            self.assertEqual(expected, actual)
+
+if __name__ == '__main__':
+    unittest.main()
